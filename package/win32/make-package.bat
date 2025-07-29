a16c423f1e53fc5e26c6b99b34bc0801ff94bae5
Jorge Guerra <67440294+jorgeoguerra@users.noreply.github.com>
2025-07-28 18:47:52 -0700

    Update make-package.bat

--- package/win32/make-package.bat
+++ package/win32/make-package.bat
@@ -170,14 +170,16 @@ if defined RSTUDIO_VERSION_SUFFIX (
     set RSTUDIO_VERSION_SUFFIX=-dev+999
 )
 
-REM Remove quotes from RSTUDIO_VERSION_SUFFIX if present
-set "RSTUDIO_VERSION_SUFFIX_CLEAN=%RSTUDIO_VERSION_SUFFIX:"=%"
-set RSTUDIO_VERSION_FULL=%RSTUDIO_VERSION_MAJOR%.%RSTUDIO_VERSION_MINOR%.%RSTUDIO_VERSION_PATCH%%RSTUDIO_VERSION_SUFFIX_CLEAN%
+set RSTUDIO_VERSION_FULL=%RSTUDIO_VERSION_MAJOR%.%RSTUDIO_VERSION_MINOR%.%RSTUDIO_VERSION_PATCH%%RSTUDIO_VERSION_SUFFIX%
 
 REM Set Electron package.json version
 pushd "%ELECTRON_SOURCE_DIR%"
 echo Setting package.json version to %RSTUDIO_VERSION_FULL%
-%NPX% json -I -f package.json -e "this.version='%RSTUDIO_VERSION_FULL%'"
+"%NODE%" -e "const fs=require('fs');const p=JSON.parse(fs.readFileSync('package.json'));p.version='%RSTUDIO_VERSION_FULL%';fs.writeFileSync('package.json',JSON.stringify(p,null,2));" || (
+    echo.!! ERROR: Failed to update package.json version
+    popd
+    exit /b 1
+)
 popd
 
 REM Set default CMake build type.
