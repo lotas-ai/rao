f245d14c74a31830ba67be15637fd11f7b1747bb
Will Nickols <willnickols@college.harvard.edu>
2025-10-01 23:46:18 -0700

    Remove ai_local_backend dist and out files from git tracking

--- src/cpp/session/resources/ai_local_backend/out/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/*---------------------------------------------------------------------------------------------
- * Copyright (c) Lotas Inc. All rights reserved.
- * Licensed under the MIT License.
- *--------------------------------------------------------------------------------------------*/
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
